d18dfdb0a4dcb690a993a3bd87ef2a599aaf146d
levvsha <levsha.mikhail@gmail.com>
2018-01-24 14:03:57 +0300

    feat: add tooltip prettify main code

--- scraper.js
+++ scraper.js
@@ -1,6 +1,6 @@
-var Xray = require('x-ray');
-var x = Xray();
-var fs = require('fs');
+const Xray = require('x-ray');
+const x = Xray();
+const fs = require('fs');
 
 x('https://stackoverflow.com/users/5806646/mikhail-shabrikov?tab=tags', '.user-tags',
   {
@@ -8,30 +8,31 @@ x('https://stackoverflow.com/users/5806646/mikhail-shabrikov?tab=tags', '.user-t
     score: x('.user-tags', ['.answer-votes'])
   }
 )
-.paginate('.user-tab-paging a@href')
-.limit(2)
-(function(error, result) {
-  if (error) {
-    return console.log('error during page scrapping', error);
-  }
-
-  var scores = {};
-
-  result.forEach(function(page) {
-    page.score.forEach(function(score, index) {
-      const scoreAsNumber = parseInt(score, 10);
-
-      if (scoreAsNumber) {
-        scores[page.tag[index]] = scoreAsNumber;
+  .paginate('.user-tab-paging a@href')
+  .limit(2)(
+    function(error, result) {
+      if (error) {
+        return console.log('error during page scrapping', error);
       }
-    })
-  });
 
-  fs.writeFileSync('./scores.json', JSON.stringify(scores), 'utf-8', function (writeFileError) {
-    if (writeFileError) {
-      return console.log('error during file writing', writeFileError);
+      const scores = {};
+
+      result.forEach(function(page) {
+        page.score.forEach(function(score, index) {
+          const scoreAsNumber = parseInt(score, 10);
+
+          if (scoreAsNumber) {
+            scores[page.tag[index]] = scoreAsNumber;
+          }
+        })
+      });
+
+      fs.writeFileSync('./scores.json', JSON.stringify(scores, null, 4), 'utf-8', function (writeFileError) {
+        if (writeFileError) {
+          return console.log('error during file writing', writeFileError);
+        }
+
+        console.log('File saved!');
+      });
     }
-
-    console.log("File saved!");
-  });
-});
+  );
--- src/script.js
+++ src/script.js
@@ -1,21 +1,23 @@
+import '../styles.styl';
+
 import * as d3 from 'd3';
+import groups from '../groupsMapping.json';
 
-
-import dataAsJson from '../scores.json';
-console.log('dataAsJson ==>', dataAsJson)
-const sizeDivisor = 100;
 const nodePadding = 2.5;
 
+fetch('https://api.stackexchange.com/2.2/users/5806646/top-answer-tags?key=U4DMV*8nvpm3EOpvf69Rxw((&site=stackoverflow&pagesize=100&filter=default')
+  .then(function(response) { return response.json(); })
+  .then(function(json) {
+    draw(json.items)
+  });
 
-export default function draw() {
-  const processedData = Object.keys(dataAsJson)
-    .map(tagName => ({ tag: tagName, score: dataAsJson[tagName] }))
+export default function draw(dataAsJson) {
+  const processedData = dataAsJson
+    .map(item => ({ tag: item.tag_name, score: item.answer_score }))
     .map(types);
 
-  console.log('processedData ==>', processedData);
   const width = window.innerWidth;
   const height = window.innerHeight;
-
 
   function dragstarted(d) {
     if (!d3.event.active) simulation.alphaTarget(.03).restart();
@@ -35,66 +37,65 @@ export default function draw() {
   }
 
   function types(d) {
-    console.log('d', d, sizeDivisor)
     d.size = d.score * 1.5;
-    d.size < 3 ? d.radius = 3 : d.radius = d.size;
+    d.size <= 5 ? d.radius = 5 : d.radius = d.size;
     return d;
   }
 
-  var svg = d3.select('body')
+  const svg = d3.select('body')
     .append('svg')
     .attr('width', width)
     .attr('height', height);
 
-  var color = d3.scaleOrdinal(["#66c2a5", "#fc8d62", "#8da0cb", "#e78ac3", "#a6d854", "#ffd92f", "#e5c494", "#b3b3b3"]);
+  const color = d3.scaleOrdinal(['#66c2a5', '#fc8d62', '#8da0cb', '#e78ac3', '#a6d854', '#ffd92f', '#e5c494', '#b3b3b3']);
 
-  var simulation = d3.forceSimulation()
-  .force("forceX", d3.forceX().strength(.1).x(width * .5))
-  .force("forceY", d3.forceY().strength(.1).y(height * .5))
-  .force("center", d3.forceCenter().x(width * .5).y(height * .5))
-  .force("charge", d3.forceManyBody().strength(-15));
+  const simulation = d3.forceSimulation()
+    .force('forceX', d3.forceX().strength(.1).x(width * .5))
+    .force('forceY', d3.forceY().strength(.1).y(height * .5))
+    .force('center', d3.forceCenter().x(width * .5).y(height * .5))
+    .force('charge', d3.forceManyBody().strength(-15));
 
+  // sort the nodes so that the bigger ones are at the back
+  //   graph = graph.sort(function(a, b) {
+  //     return b.size - a.size;
+  //   });
 
-// sort the nodes so that the bigger ones are at the back
-//   graph = graph.sort(function(a, b) {
-//     return b.size - a.size;
-//   });
-
-//update the simulation based on the data
+  // update the simulation based on the data
   simulation
-  .nodes(processedData)
-  .force("collide", d3.forceCollide().strength(.5).radius(function(d) {
-    return d.radius + nodePadding;
-  }).iterations(1))
-  .on("tick", function(d) {
-    node
-    .attr("cx", function(d) {
+    .nodes(processedData)
+    .force('collide', d3.forceCollide().strength(.5).radius(function(d) {
+      return d.radius + nodePadding;
+    }).iterations(1))
+    .on('tick', function() {
+      node
+        .attr('cx', function(d) {
+          return d.x;
+        })
+        .attr('cy', function(d) {
+          return d.y;
+        })
+    });
+
+  const node = svg.append('g')
+    .attr('class', 'node')
+    .selectAll('circle')
+    .data(processedData)
+    .enter().append('circle')
+    .attr('r', function(d) {
+      return d.radius;
+    })
+    .attr('fill', function(d) {
+      return color(groups[d.tag]);
+    })
+    .attr('cx', function(d) {
       return d.x;
     })
-    .attr("cy", function(d) {
+    .attr('cy', function(d) {
       return d.y;
     })
-  });
-
-  var node = svg.append("g")
-  .attr("class", "node")
-  .selectAll("circle")
-  .data(processedData)
-  .enter().append("circle")
-  .attr("r", function(d) {
-    return d.radius;
-  })
-  .attr("fill", function(d) {
-    return color(d.continent);
-  })
-  .attr("cx", function(d) {
-    return d.x;
-  })
-  .attr("cy", function(d) {
-    return d.y;
-  })
-  .call(d3.drag()
-  .on("start", dragstarted)
-  .on("drag", dragged)
-  .on("end", dragended));
+    .call(d3.drag()
+      .on('start', dragstarted)
+      .on('drag', dragged)
+      .on('end', dragended)
+    );
 }
