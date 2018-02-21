e50c24f6aa859e0cf43badbf6fd20f48cdd39b10
levvsha <levsha.mikhail@gmail.com>
2018-02-21 21:43:44 +0300

    refactor: grouping entities by their intended roles

--- src/script.js
+++ src/script.js
@@ -24,11 +24,6 @@ const height = window.innerHeight;
 const MAX_RADIUS = 150;
 const MAX_AREA = Math.pow(MAX_RADIUS , 2) * Math.PI;
 
-const svg = d3.select('body')
-  .append('svg')
-  .attr('width', width)
-  .attr('height', height);
-
 document.getElementById('js-send-button').addEventListener('click', () => {
   svg.html('');
   fetchAndDraw(input.value.split('/')[4], true);
@@ -36,66 +31,102 @@ document.getElementById('js-send-button').addEventListener('click', () => {
 
 fetchAndDraw();
 
+const svg = d3.select('body')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height);
+
 class Vizualization {
-  constructor(data, useColorByScore) {
-    this.drawCircles();
+  constructor(rawData, useColorByScore) {
+    this.nodes = {
+      rootSvg: svg,
+      circleGroup: null,
+      circles: null,
+      tooltip: d3.select('.js-tooltip'),
+      tagLinks: d3.selectAll('.js-tag-link')
+    };
+
+    this.scales = {
+      circleAreaScale: null,
+      colorScale: null,
+      colorScheme: null
+    };
+
+    this.data = this.getProcessedData(rawData);
+
+    this.moveTooltip = _throttle(this.moveTooltip, 300);
+
+    this.calculateScales();
+    this.drawCircles(this.data); // drawSkeleton
+    this.fillWithData();
+    this.bindEvents();
   }
 
-  drawCircles(dataAsJson, useColorByScore) {
+  bindEvents() {
+
+  }
+
+  calculateScales() {
+
+  }
+
+  getProcessedData(dataAsJson) {
+    return dataAsJson
+      .map(item => ({ tag: item.tag_name, score: item.answer_score }))
+      .map(d => {
+        d.size =  d.score;
+        d.radius = Math.sqrt(this.scales.circleAreaScale(d.score) / Math.PI);
+
+        return d;
+      });
+  }
+
+  moveTooltip(left, top) {
+    this.nodes.tooltip
+      .style('left', `${ left }px`)
+      .style('top', `${ top }px`);
+  }
+
+  drawCircles(data, useColorByScore) {
     let isDragged = false;
 
-    const areaScaleDomain = d3.extent(dataAsJson, d => d.answer_score);
+    const areaScaleDomain = d3.extent(data, d => d.score);
 
-    const areaScale = d3.scaleLinear()
+    this.scales.circleAreaScale = d3.scaleLinear()
       .domain(areaScaleDomain)
       .range([MAX_AREA / (areaScaleDomain[1] / areaScaleDomain[0]), MAX_AREA]);
 
-    const processedData = dataAsJson
-      .map(item => ({ tag: item.tag_name, score: item.answer_score }))
-      .map(types);
-
     const upperValueForScale = _ceil(areaScaleDomain[1], -1);
 
-    this.colorScale = d3.scaleLinear()
+    this.scales.colorScale = d3.scaleLinear()
       .domain([0, upperValueForScale / 2, upperValueForScale])
       .range(['rgb(34, 131, 187)', 'rgb(253, 255, 140)', 'rgb(216, 31, 28)']);
 
-    const color = d3.scaleOrdinal(d3.schemeCategory20);
+    this.scales.colorScheme = d3.scaleOrdinal(d3.schemeCategory20);
 
-    const getColorByScore = (d) => colorScale(d.score);
-    const getColorByTag = (d) => (color(groups[d.tag] || 'other'));
+    this.getColorByScore = (d) => this.scales.colorScale(d.score);
+    this.getColorByTag = (d) => (this.scales.colorScheme(groups[d.tag] || 'other'));
 
-    drawLegend(upperValueForScale);
+    // drawLegend(upperValueForScale);
 
-    const tooltip = d3.select('.js-tooltip');
-
-    const simulation = d3.forceSimulation()
+    this.simulation = d3.forceSimulation()
       .force('forceX', d3.forceX().strength(.1).x(width * .5))
       .force('forceY', d3.forceY().strength(.1).y(height * .5))
       .force('center', d3.forceCenter().x(width * .5).y(height * .5))
       .force('charge', d3.forceManyBody().strength(-15));
 
-    simulation
-      .nodes(processedData)
-      .force('collide', d3.forceCollide().strength(.5).radius(function(d) {
-        return d.radius + 2.5;
-      }).iterations(1))
-      .on('tick', function() {
-        nodes
-          .attr('transform', function(d) {
-            return `translate(${ d.x },${ d.y })`;
-          });
+    this.simulation
+      .nodes(this.data)
+      .force('collide', d3.forceCollide().strength(.5).radius(d => d.radius + 2.5).iterations(1))
+      .on('tick', () => {
+        this.nodes.circleGroup
+          .attr('transform', d => `translate(${ d.x },${ d.y })`);
       });
 
-    const moveTooltip = _throttle((left, top) => {
-      tooltip.style('left', `${ left }px`)
-        .style('top', `${ top }px`);
-    }, 300);
-
-    const nodes = svg.append('g')
+    this.nodes.circleGroup = svg.append('g')
       .attr('class', 'nodes')
       .selectAll('.node')
-      .data(processedData)
+      .data(this.processedData)
       .enter()
       .append('g')
       .attr('class', 'node')
@@ -104,16 +135,16 @@ class Vizualization {
       })
       .on('mouseenter', (d) => {
         if (!isDragged) {
-          tooltip.classed('show', true)
+          this.nodes.tooltip.classed('show', true)
             .text(`${ d.tag }: ${ d.score }`);
         }
       })
       .on('mouseleave', () => {
-        tooltip.classed('show', false);
+        this.nodes.tooltip.classed('show', false);
       })
       .on('mousemove', () => {
         const { clientX, clientY } = d3.event;
-        moveTooltip(clientX, clientY);
+        this.moveTooltip(clientX, clientY);
       })
       .call(d3.drag()
         .on('start', dragstarted)
@@ -121,13 +152,13 @@ class Vizualization {
         .on('end', dragended)
       );
 
-    const circles = nodes.append('circle')
+    this.nodes.circles = this.nodes.circleGroup.append('circle')
       .attr('r', function(d) {
         return d.radius;
       })
-      .attr('fill', /* useColorByScore ? */ getColorByScore /* : getColorByTag */);
+      .attr('fill', /* useColorByScore ? */ this.getColorByScore /* : getColorByTag */);
 
-    nodes
+    this.nodes.circleGroup
       .append('text')
       .text(function(d) { return d.tag; })
       .style('font-size', function(d) {
@@ -141,7 +172,7 @@ class Vizualization {
       })
       .attr('dy', '.35em');
 
-    d3.selectAll('.js-tag-link')
+    this.nodes.tagLinks
       .on('mouseenter', function() {
         const tag = this.getAttribute('data-tag');
         animateCircles(tag);
@@ -149,7 +180,7 @@ class Vizualization {
       .on('mouseleave', function() {
         const tag = this.getAttribute('data-tag');
 
-        circles.filter(function(d) {
+        this.nodes.circles.filter(function(d) {
           return groups[d.tag] === tag
         })
           .transition()
@@ -160,7 +191,7 @@ class Vizualization {
       });
 
     function animateCircles(tag) {
-      circles.filter(function(d) {
+      this.nodes.circles.filter(function(d) {
         return groups[d.tag] === tag
       })
         .each(function() {
@@ -183,10 +214,13 @@ class Vizualization {
     }
 
     function dragstarted(d) {
-      tooltip.classed('show', false);
+      this.nodes.tooltip.classed('show', false);
       isDragged = true;
 
-      if (!d3.event.active) simulation.alphaTarget(.03).restart();
+      if (!d3.event.active) {
+        this.simulation.alphaTarget(.03).restart();
+      }
+
       d.fx = d.x;
       d.fy = d.y;
     }
@@ -198,16 +232,13 @@ class Vizualization {
 
     function dragended(d) {
       isDragged = false;
-      if (!d3.event.active) simulation.alphaTarget(.03);
+
+      if (!d3.event.active) {
+        this.simulation.alphaTarget(.03);
+      }
+
       d.fx = null;
       d.fy = null;
-    }
-
-    function types(d) {
-      d.size =  d.score;
-      d.radius = Math.sqrt(areaScale(d.score) / Math.PI);
-
-      return d;
     }
   }
 
@@ -216,73 +247,71 @@ class Vizualization {
   }
 }
 
-function draw(dataAsJson, useColorByScore) {
+// function draw(dataAsJson, useColorByScore) {
 
-
-  function drawLegend(upperValueForScale) {
-    const legendWidth = 600;
-
-    const countScale = d3.scaleLinear()
-      .domain([0, upperValueForScale])
-      .range([0, legendWidth]);
-
-    const numStops = 10;
-    const countRange = countScale.domain();
-    countRange[2] = countRange[1] - countRange[0];
-    const countPoint = [];
-    for(var i = 0; i < numStops; i++) {
-      countPoint.push(i * countRange[2] / (numStops - 1) + countRange[0]);
-    }
-
-    svg.append('defs')
-      .append('linearGradient')
-      .attr('id', 'legend-traffic')
-      .attr('x1', '0%').attr('y1', '0%')
-      .attr('x2', '100%').attr('y2', '0%')
-      .selectAll('stop')
-      .data(d3.range(10))
-      .enter().append('stop')
-      .attr('offset', function(d,i) {
-        return countScale( countPoint[i] )/legendWidth;
-      })
-      .attr('stop-color', function(d,i) {
-        return colorScale( countPoint[i] );
-      });
-
-    const legendsvg = svg.append('g')
-      .attr('class', 'legendWrapper')
-      .attr('transform', 'translate(' + (width/2) + ',80)');
-
-    legendsvg.append('rect')
-      .attr('class', 'legendRect')
-      .attr('x', -legendWidth/2)
-      .attr('y', 0)
-      .attr('width', legendWidth)
-      .attr('height', 10)
-      .style('fill', 'url(#legend-traffic)');
-
-    legendsvg.append('text')
-      .attr('class', 'legendTitle')
-      .attr('x', 0)
-      .attr('y', -10)
-      .style('text-anchor', 'middle')
-      .text('Scores');
-
-    const xScale = d3.scaleLinear()
-      .range([-legendWidth/2, legendWidth/2])
-      .domain([0, upperValueForScale]);
-
-    const xAxis = d3.axisBottom()
-      .scale(xScale)
-      .tickValues(xScale.ticks(4).concat(xScale.domain()));
-
-    legendsvg.append('g')
-      .attr('class', 'axis')
-      .attr('transform', 'translate(0,' + (10) + ')')
-      .call(xAxis);
-  }
-
-  function update() {
-    // const nodes
-  }
-}
+// function drawLegend(upperValueForScale) {
+//   const legendWidth = 600;
+//
+//   const countScale = d3.scaleLinear()
+//     .domain([0, upperValueForScale])
+//     .range([0, legendWidth]);
+//
+//   const numStops = 10;
+//   const countRange = countScale.domain();
+//   countRange[2] = countRange[1] - countRange[0];
+//   const countPoint = [];
+//   for(var i = 0; i < numStops; i++) {
+//     countPoint.push(i * countRange[2] / (numStops - 1) + countRange[0]);
+//   }
+//
+//   svg.append('defs')
+//     .append('linearGradient')
+//     .attr('id', 'legend-traffic')
+//     .attr('x1', '0%').attr('y1', '0%')
+//     .attr('x2', '100%').attr('y2', '0%')
+//     .selectAll('stop')
+//     .data(d3.range(10))
+//     .enter().append('stop')
+//     .attr('offset', function(d,i) {
+//       return countScale( countPoint[i] )/legendWidth;
+//     })
+//     .attr('stop-color', function(d,i) {
+//       return this.scales.colorScale( countPoint[i] );
+//     });
+//
+//   const legendsvg = svg.append('g')
+//     .attr('class', 'legendWrapper')
+//     .attr('transform', 'translate(' + (width/2) + ',80)');
+//
+//   legendsvg.append('rect')
+//     .attr('class', 'legendRect')
+//     .attr('x', -legendWidth/2)
+//     .attr('y', 0)
+//     .attr('width', legendWidth)
+//     .attr('height', 10)
+//     .style('fill', 'url(#legend-traffic)');
+//
+//   legendsvg.append('text')
+//     .attr('class', 'legendTitle')
+//     .attr('x', 0)
+//     .attr('y', -10)
+//     .style('text-anchor', 'middle')
+//     .text('Scores');
+//
+//   const xScale = d3.scaleLinear()
+//     .range([-legendWidth/2, legendWidth/2])
+//     .domain([0, upperValueForScale]);
+//
+//   const xAxis = d3.axisBottom()
+//     .scale(xScale)
+//     .tickValues(xScale.ticks(4).concat(xScale.domain()));
+//
+//   legendsvg.append('g')
+//     .attr('class', 'axis')
+//     .attr('transform', 'translate(0,' + (10) + ')')
+//     .call(xAxis);
+// }
+// function update() {
+//   const nodes
+// }
+// }
